0c2f8048b77a70ecf804b163138b137ecac65f99
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2017-06-19 13:48:22 +0200

    reliable dir creation

--- src/main/java/com/trivago/rta/utils/FileUtils.java
+++ src/main/java/com/trivago/rta/utils/FileUtils.java
@@ -87,11 +87,13 @@ public final class FileUtils {
 
     /**
      * Creates a directory if it does not exists
+     *
      * @param dirName Name of directory.
-     * @return true if directory exists or was created.
      */
-    public static boolean createDir(String dirName){
+    public static void createDir(String dirName) {
         File directory = new File(dirName);
-        return directory.exists() || directory.mkdir();
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
     }
 }
