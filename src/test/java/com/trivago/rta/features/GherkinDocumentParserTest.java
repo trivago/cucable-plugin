5c6e2e720026284d516ec93095940ae14db9f42a
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2017-09-14 14:30:14 +0200

    added document parser test

--- src/test/java/com/trivago/rta/features/GherkinDocumentParserTest.java
+++ src/test/java/com/trivago/rta/features/GherkinDocumentParserTest.java
@@ -1,5 +1,55 @@
 package com.trivago.rta.features;
 
+import gherkin.ast.Comment;
+import gherkin.ast.Feature;
+import gherkin.ast.GherkinDocument;
+import gherkin.ast.Location;
+import gherkin.ast.ScenarioDefinition;
+import gherkin.ast.Step;
+import gherkin.ast.Tag;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class GherkinDocumentParserTest {
-    // tbd
+
+    private GherkinDocumentParser gherkinDocumentParser = new GherkinDocumentParser();
+
+    @Test
+    public void oneKeywordScenario() throws Exception {
+        ScenarioDefinition mockScenario = mock(ScenarioDefinition.class);
+
+        List<Step> steps = new ArrayList<>();
+        Step step1 = new Step(new Location(1, 1), "given ", "this is step 1", null);
+        steps.add(step1);
+        when(mockScenario.getSteps()).thenReturn(steps);
+        GherkinDocument gherkinDocument = getGherkinDocument(mockScenario);
+
+        List<List<String>> keywordsFromGherkinDocument = gherkinDocumentParser.getKeywordsFromGherkinDocument(gherkinDocument);
+        assertThat(keywordsFromGherkinDocument.size(), is(1));
+        assertThat(keywordsFromGherkinDocument.get(0).size(), is(1));
+        assertThat(keywordsFromGherkinDocument.get(0).get(0), is("given"));
+    }
+
+    private GherkinDocument getGherkinDocument(final ScenarioDefinition scenarioDefinition) {
+        List<Tag> tags = new ArrayList<>();
+        Location location = new Location(11, 22);
+        String language = "language";
+        String keyword = "keyword";
+        String name = "name";
+        String description = "description";
+
+        List<ScenarioDefinition> children = new ArrayList<>();
+        children.add(scenarioDefinition);
+
+        Feature feature = new Feature(tags, location, language, keyword, name, description, children);
+        List<Comment> comments = new ArrayList<>();
+        return new GherkinDocument(feature, comments);
+    }
 }
